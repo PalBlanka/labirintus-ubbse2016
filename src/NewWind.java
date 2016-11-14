958c2db58b1ca15f24b127b19eb4999e3a32222f
Elod Balint <balintelod@freemail.hu>
2016-11-14 22:50:08 +0200

    javitottuk a menut most mar nem lapoz

--- src/NewWind.java
+++ src/NewWind.java
@@ -16,24 +16,28 @@ import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
 public class NewWind {
-	private JFrame frame1 = new JFrame();
+	private JFrame foframe = new JFrame();
 	private JPanel fopanel = new JPanel();
 	//hatteret tartalmazza
-	private JPanel jPanel1 = new JPanel();
 	//Fo menut tartalmazza
-	private JPanel jPanel2 = new JPanel();
+	private JPanel fomenuPanel =new JPanel();
 	//Quest menut tartalmazza
-	private JPanel jPanel3 = new JPanel();
+	private JPanel loginPanel = new JPanel();
 	//Login menut tartalmazza
-	private JPanel jPanel4 = new JPanel();
+	private JPanel questPanel = new JPanel();
+	//Register
+	private JPanel registerPanel = new JPanel();
 	
 	//bbe a alabelbe szurjuk be a buffered imaget
-	 JLabel jLabelPic =  new JLabel();
-	
+	 JLabel jLabelPicFomenu =  new JLabel();
+	 JLabel jLabelPicQuest =  new JLabel();
+	 JLabel jLabelPicLogin =  new JLabel();
+	 JLabel jLabelPicRegister =  new JLabel();
+	 
 	//Fõ menü
-	JButton jButton1 = new JButton();
-	JButton jButton2 = new JButton();
-	JButton jButton3 = new JButton();
+	JButton loginButton = new JButton();
+	JButton registrationButton = new JButton();
+	JButton questButton = new JButton();
 	
 	//Quest
 	JButton jButton4 = new JButton();
@@ -46,6 +50,20 @@ public class NewWind {
 	JTextField jTextField2 = new JTextField();
 	JButton jButton8 = new JButton();
 	JButton jButton9 = new JButton();
+	JLabel jLabelLoginUserName = new JLabel();
+	JLabel jLabelLoginPassword = new JLabel();
+	
+	//Register
+		JTextField jTextFieldRegisterUserName = new JTextField();
+		JTextField jTextFieldRegisterEmail = new JTextField();
+		JTextField jTextFieldRegisterPassword1 = new JTextField();
+		JTextField jTextFieldRegisterPassword2 = new JTextField();
+		JLabel jLabelRegistrationUserName = new JLabel();
+		JLabel jLabelRegistrationEmail = new JLabel();
+		JLabel jLabelRegisterPassword = new JLabel();
+		JLabel jLabelRegisterPasswordAgain = new JLabel();
+		JButton jButtonRegisterBack = new JButton();
+		JButton jButtonRegisterAccept = new JButton();
 	
 	
 	//ezzel egy üres képet készitünk, amire késõbb rátesszük a nekünk szükséges képet
@@ -57,52 +75,49 @@ public class NewWind {
 	//ez fogja a kezdõ ablakot kirajzolni
 	public NewWind(){
 		//létrehozunk egy új keretet
-		frame1 = new JFrame();
+		foframe = new JFrame();
 		//keret cimének beálitása
-		frame1.setTitle("Labirintus");
+		foframe.setTitle("Labirintus");
 		//x, y, width, height(keret méretének beálitása)
-		frame1.setBounds(500,100,405,430);
+		foframe.setBounds(500,100,405,430);
 		//beálitjuk, hogy mikor x re kattintunk az ablak bezárodjon
-		frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		foframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		//igy barhova szurhatok be elemeket 
-		frame1.setLayout(null);
+		foframe.setLayout(null);
 		
 		//itt betesszük az alappanelt, erre fog kerülni az összes többi
 		//ezután csak a fopanelre tesszük a többi komponenst
-		fopanel =(JPanel) frame1.getContentPane();
+		fopanel =(JPanel) foframe.getContentPane();
 		
-		//ez fogja tartalmazni a képet
-		jPanel1= new JPanel();
-		jPanel1.setBounds(0, 0, 390, 400);
-		//itt is bekell állitani a setLayoutut nullra, annak érdekében, hogy kedvünk szerint mozgathassuk a komponenseket
-		// ha nem állitjuk be, akkor õ fogja rendezni.
-		jPanel1.setLayout(null);
-		//Panel háttérszinének beálitása
-		jPanel1.setBackground(Color.LIGHT_GRAY);
 		
 		//ez fogja tartalmazni a fõ menüt.
-		jPanel2= new JPanel();
-		jPanel2.setBounds(0, 0, 390, 400);
-		jPanel2.setLayout(null);
-		jPanel2.setBackground(Color.LIGHT_GRAY);
+		fomenuPanel= new JPanel();
+		fomenuPanel.setBounds(0, 0, 390, 400);
+		fomenuPanel.setLayout(null);
+		fomenuPanel.setBackground(Color.LIGHT_GRAY);
 		
 		
 		//ez a panel tartalmazza a Quest menüt
-		jPanel3= new JPanel();
-		jPanel3.setBounds(0, 0, 390, 400);
-		jPanel3.setLayout(null);
-		jPanel3.setBackground(Color.LIGHT_GRAY);
+		loginPanel= new JPanel();
+		loginPanel.setBounds(0, 0, 390, 400);
+		loginPanel.setLayout(null);
+		loginPanel.setBackground(Color.LIGHT_GRAY);
 		
 		//ez a panel tartalmazza a Login menüt
-		jPanel4= new JPanel();
-		jPanel4.setBounds(0, 0, 390, 400);
-		jPanel4.setLayout(null);
-		jPanel4.setBackground(Color.LIGHT_GRAY);
+		questPanel= new JPanel();
+		questPanel.setBounds(0, 0, 390, 400);
+		questPanel.setLayout(null);
+		questPanel.setBackground(Color.LIGHT_GRAY);
+		
+		//ez a panel tartalmazza a Register menüt
+		registerPanel= new JPanel();
+		registerPanel.setBounds(0, 0, 390, 400);
+		registerPanel.setLayout(null);
+		registerPanel.setBackground(Color.LIGHT_GRAY);
 				
 		
 		//A JLabel azért kell nekünk, mert ennek segitségével adhatunk háttér képet
 		// a Label Iconja lesz a kép, és a setIcon parancsal álithatjuk be
-		jLabelPic = new JLabel();
 		
 		
 		//itt adjuk hozza az url alapjan a kepet
@@ -119,27 +134,37 @@ public class NewWind {
 		}
        
 		
-		jLabelPic.setIcon(new ImageIcon(startImage));
-		jLabelPic.setBounds(0, 0,390,400);
+		jLabelPicFomenu.setIcon(new ImageIcon(startImage));
+		jLabelPicFomenu.setBounds(0, 0,390,400);
 		
+		jLabelPicQuest.setIcon(new ImageIcon(startImage));
+		jLabelPicQuest.setBounds(0, 0,390,400);
+		
+		
+		jLabelPicLogin.setIcon(new ImageIcon(startImage));
+		jLabelPicLogin.setBounds(0, 0,390,400);
+		
+		jLabelPicRegister.setIcon(new ImageIcon(startImage));
+		jLabelPicRegister.setBounds(0, 0,390,400);
 		
 		
 	
 		//gombok szövegének, illetve méretének beálitása
 		//ez a kezdokepernyo gombjai
-		jButton1 = new JButton("Login");
-        jButton1.setBounds(120, 100, 150, 40);
+		loginButton = new JButton("Login");
+		loginButton.setBounds(120, 100, 150, 40);
        
-        jButton2 = new JButton("Registration");
-        jButton2.setBounds(120, 150, 150, 40);
+		registrationButton = new JButton("Registration");
+		registrationButton.setBounds(120, 150, 150, 40);
         
-        jButton3 = new JButton("Quest");
-        jButton3.setBounds(120, 200, 150, 40);
+		questButton = new JButton("Quest");
+		questButton.setBounds(120, 200, 150, 40);
         
         //komponensek egybefûzése a kezdo kepernyon
-        jPanel2.add(jButton1);
-		jPanel2.add(jButton2);
-		jPanel2.add(jButton3);
+        fomenuPanel.add(loginButton);
+        fomenuPanel.add(registrationButton);
+        fomenuPanel.add(questButton);
+        fomenuPanel.add(jLabelPicFomenu);
         
 		
 		
@@ -158,22 +183,29 @@ public class NewWind {
 		jButton7.setBounds(120, 200, 150, 40);
 		        
 		//komponensek egybefûzése a quest kepernyon
-		jPanel3.add(jButton4);
-		jPanel3.add(jButton5);
-		jPanel3.add(jButton6);
-		jPanel3.add(jButton7);
+		questPanel.add(jButton4);
+		questPanel.add(jButton5);
+		questPanel.add(jButton6);
+		questPanel.add(jButton7);
+		questPanel.add(jLabelPicQuest);
 		
 				
 		//komponensek bealitas a Login menuben
 		
 		//itt kozepre helyezzuk a szoveget
-		jTextField1 = new JTextField("User Name", SwingConstants.CENTER);
-		jTextField1.setBounds(120, 100, 160, 30);
+		jTextField1 = new JTextField();
+		jTextField1.setBounds(160, 100, 160, 30);
 		
-		jTextField2 = new JTextField("Password", SwingConstants.CENTER);
-		jTextField2.setBounds(120, 140, 160, 30);
+		jLabelLoginUserName =new JLabel("User Name", SwingConstants.CENTER);
+		jLabelLoginUserName.setBounds(50, 100, 110, 30);
+		jLabelLoginUserName.setForeground(Color.WHITE);
 		
+		jTextField2 = new JTextField();
+		jTextField2.setBounds(160, 140, 160, 30);
 		
+		jLabelLoginPassword =new JLabel("Password", SwingConstants.CENTER);
+		jLabelLoginPassword.setBounds(50, 140, 110, 30);
+		jLabelLoginPassword.setForeground(Color.WHITE);
 		
 		jButton8 = new JButton("Back");
 		jButton8.setBounds(40, 230, 150, 40);
@@ -181,25 +213,81 @@ public class NewWind {
 		jButton9.setBounds(200, 230, 150, 40);
 		
 	
-		jPanel4.add(jTextField1);
-		jPanel4.add(jTextField2);
-		jPanel4.add(jButton8);
-		jPanel4.add(jButton9);
+		loginPanel.add(jTextField1);
+		loginPanel.add(jTextField2);
+		loginPanel.add(jLabelLoginUserName);
+		loginPanel.add(jLabelLoginPassword);
+		loginPanel.add(jButton8);
+		loginPanel.add(jButton9);
+		loginPanel.add(jLabelPicLogin);
+	
 		
-        fopanel.add(jPanel1);
-       fopanel.add(jPanel2);
-		jPanel1.add(jLabelPic);
+		//itt van a register menu
+		jTextFieldRegisterUserName = new JTextField();
+		jTextFieldRegisterUserName.setBounds(160, 50, 160, 30);
+		
+		jLabelRegistrationUserName =new JLabel("User Name", SwingConstants.CENTER);
+		jLabelRegistrationUserName.setBounds(50, 50, 110, 30);
+		jLabelRegistrationUserName.setForeground(Color.WHITE);
+		
+		jTextFieldRegisterEmail = new JTextField();
+		jTextFieldRegisterEmail.setBounds(160, 90, 160, 30);
+		
+		jLabelRegistrationEmail =new JLabel("Email", SwingConstants.CENTER);
+		jLabelRegistrationEmail.setBounds(50, 90, 110, 30);
+		jLabelRegistrationEmail.setForeground(Color.WHITE);
+		
+		jTextFieldRegisterPassword1 = new JTextField();
+		jTextFieldRegisterPassword1.setBounds(160, 130, 160, 30);
+				
+		jLabelRegisterPassword =new JLabel("Password", SwingConstants.CENTER);
+		jLabelRegisterPassword.setBounds(50, 130, 110, 30);
+		jLabelRegisterPassword.setForeground(Color.WHITE);
+		
+		jTextFieldRegisterPassword2 = new JTextField();
+		jTextFieldRegisterPassword2.setBounds(160, 170, 160, 30);
+		
+		jLabelRegisterPasswordAgain =new JLabel("Password Again", SwingConstants.CENTER);
+		jLabelRegisterPasswordAgain.setBounds(50, 170, 110, 30);
+		jLabelRegisterPasswordAgain.setForeground(Color.WHITE);
+				
+				
+		jButtonRegisterBack = new JButton("Back");
+		jButtonRegisterBack.setBounds(40, 230, 150, 40);
+		jButtonRegisterAccept = new JButton("Accept");
+		jButtonRegisterAccept.setBounds(200, 230, 150, 40);
+				
+			
+
+		registerPanel.add(jTextFieldRegisterUserName);
+		registerPanel.add(jLabelRegistrationUserName);
+		registerPanel.add(jLabelRegistrationEmail);
+		registerPanel.add(jTextFieldRegisterEmail);
+		registerPanel.add(jTextFieldRegisterPassword1);
+		registerPanel.add(jLabelRegisterPassword);
+		registerPanel.add(jTextFieldRegisterPassword2);
+		registerPanel.add(jLabelRegisterPasswordAgain);
+		registerPanel.add(jButtonRegisterBack);
+		registerPanel.add(jButtonRegisterAccept);
+		registerPanel.add(jLabelPicRegister);
+		
+			
+		
+		
+        //fopanel.add(rajzPanel1);
+       fopanel.add(fomenuPanel);
+       //rajzPanel1.add(jLabelPic);
 		
 		
 		//a keret láthatóvá tétele
-		frame1.setVisible(true);
+		foframe.setVisible(true);
 	
 		
 		//Quest gomb lekezelese
-		jButton3.addActionListener((ActionEvent e) -> {
+		questButton.addActionListener((ActionEvent e) -> {
 			//eltavolitsuk a fõ menut tartalmazo panelt, s beszurjuk a guset menut tartalmazot
-			fopanel.remove(jPanel2);
-			fopanel.add(jPanel3);
+			fopanel.remove(fomenuPanel);
+			fopanel.add(questPanel);
 			fopanel.revalidate();
 			fopanel.repaint();
         });
@@ -207,17 +295,17 @@ public class NewWind {
 		//Back gomb lekezelese a quest menu alatt
 		jButton7.addActionListener((ActionEvent e) -> {
 			//eltavolitsuk a quest menut tartalmazo panelt, s beszurjuk a fõ menut tartalmazot
-			fopanel.remove(jPanel3);
-			fopanel.add(jPanel2);
+			fopanel.remove(questPanel);
+			fopanel.add(fomenuPanel);
 			fopanel.revalidate();
 			fopanel.repaint();    
         });
 		
 		//Login gomb lekezelese
-		jButton1.addActionListener((ActionEvent e) -> {
+		loginButton.addActionListener((ActionEvent e) -> {
 		//eltavolitsuk a fõ menut tartalmazo panelt, s beszurjuk a guset menut tartalmazot
-		fopanel.remove(jPanel2);
-		fopanel.add(jPanel4);
+		fopanel.remove(fomenuPanel);
+		fopanel.add(loginPanel);
 		fopanel.revalidate();
 		fopanel.repaint();
 		});
@@ -225,8 +313,26 @@ public class NewWind {
 		//Login/Back gomb lekezelese
 		jButton8.addActionListener((ActionEvent e) -> {
 		//eltavolitsuk a fõ menut tartalmazo panelt, s beszurjuk a guset menut tartalmazot
-		fopanel.remove(jPanel4);
-		fopanel.add(jPanel2);
+		fopanel.remove(loginPanel);
+		fopanel.add(fomenuPanel);
+		fopanel.revalidate();
+		fopanel.repaint();
+		});
+		
+		//Register gomb lekezelese
+		registrationButton.addActionListener((ActionEvent e) -> {
+		//eltavolitsuk a fõ menut tartalmazo panelt, s beszurjuk a guset menut tartalmazot
+		fopanel.remove(fomenuPanel);
+		fopanel.add(registerPanel);
+		fopanel.revalidate();
+		fopanel.repaint();
+		});
+				
+		//Register/Back gomb lekezelese
+		jButtonRegisterBack.addActionListener((ActionEvent e) -> {
+		//eltavolitsuk a fõ menut tartalmazo panelt, s beszurjuk a guset menut tartalmazot
+		fopanel.remove(registerPanel);
+		fopanel.add(fomenuPanel);
 		fopanel.revalidate();
 		fopanel.repaint();
 		});
